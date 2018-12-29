66651f0cb5233803fd3a6a15bd3c51fd7827ef9f
PerepelitsunaEE <43075958+PerepelitsunaEE@users.noreply.github.com>
2018-12-29 09:11:13 +0400

    Add files via upload

---
+++ IBoat.java
@@ -0,0 +1,10 @@
+import java.awt.Graphics;
+
+public interface IBoat {
+	
+	void SetPosition(int x, int y, int width, int height);
+
+	void MoveTransport(Direction direction);
+
+	void DrawSail(Graphics g);
+}
